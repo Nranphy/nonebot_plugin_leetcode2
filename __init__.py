19041124de9f208199c7cd841fa7cbb4300c7f91
Nranphy <30568146+Nranphy@users.noreply.github.com>
2022-02-18 12:06:51 +0800

    Test.

--- __init__.py
+++ __init__.py
@@ -5,8 +5,11 @@ from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.log import logger
 from .get_data import get_today_title,get_sub_problem_data
 
+
+
 global_config = get_driver().config
 config = Config.parse_obj(global_config.dict())
+
 
 request_today = on_command("每日一题",aliases={"lc","leetcode"},priority = 10,block = True)
 
@@ -18,4 +21,4 @@ async def send_today_problem(bot: Bot,event:Event):
     today_data = get_sub_problem_data(today_title)
     logger.info("获取题目内容成功。")
     logger.debug(f"题目{today_data[0]}的难度为{today_data[1]},内容略。")
-    await request_today.send("\n".join(today_data[:2])+Message.img(today_data[2])+f"\n{today_data[3]}")
+    await request_today.send("\n".join(today_data[:2])+today_data[2]+f"\n{today_data[3]}")
--- get_data.py
+++ get_data.py
@@ -1,8 +1,9 @@
 import httpx
 import json
+import os
 from nonebot_plugin_htmlrender import get_new_page
 
-#获取今日的每日一题标题，之后再另外查询内容
+#获取今日的每日一题标题，之后再另查询内容
 def get_today_title():
     try:
         get_today_data = httpx.post("https://leetcode-cn.com/graphql", json={
@@ -31,16 +32,18 @@ def get_sub_problem_data(titleSlug_):
         problem_data = problem_data["data"]["question"]
         #题目信息(题号+题目译名)
         problem_title = problem_data.get("questionFrontendId")+"."+problem_data.get("translatedTitle")
-        #题目难度
+        #题目难度(英语单词)
         problem_difficulty = "题目难度："+problem_data.get("difficulty")
         #题目内容（用html输出）
         problem_content = problem_data.get("translatedContent")
         ##去除转义
         problem_content.replace('\"','"')
         ##将html转为图片
-        with open(f"/data/nonebot_plugin_leetcode/html/{titleSlug_}.html","w+") as f:
+        html_path = f"data/nonebot_plugin_leetcode/html"
+        check_dir(html_path)
+        with open(html_path+f"/{titleSlug_}.html","w+") as f:
             f.write(problem_content)
-        problem_content = html2img(f"/data/nonebot_plugin_leetcode/html/{titleSlug_}.html",titleSlug_)
+        problem_content = html2img(html_path+f"/{titleSlug_}.html",titleSlug_)
         #题目链接
         problem_link = "本题链接："+f"https://leetcode-cn.com/problems/{titleSlug_}/" 
         return [problem_title,problem_difficulty,problem_content,problem_link]
@@ -49,15 +52,25 @@ def get_sub_problem_data(titleSlug_):
 
     
 
-#直接用大佬的插件转化html！！！！
+#直接用大佬的插件转化html！！！！yyds
 async def html2img(path,title):
     try:
         async with get_new_page(viewport={"width": 300, "height": 300}) as page:
             await page.goto(
-                path,
+                "file://"+path,
                 wait_until="networkidle"
             )
-            pic = await page.screenshot(full_page=True, path=f"/data/nonebot_plugin_leetcode/html/{title}.png")
+            img_path = f"data/nonebot_plugin_leetcode/img"
+            check_dir(img_path)
+            pic = await page.screenshot(full_page=True, path=img_path+f"/{title}.png")
             return pic
     except Exception as e:
         raise e
+
+
+#网上找来的，创建不存在的目录
+def check_dir(path):
+    '''判断文件夹是否存在，如果不存在就创建一个'''
+    if not os.path.isdir(path):
+        os.makedirs(path)
+        return "Create Successful."
