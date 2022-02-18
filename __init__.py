40acd34dfdc2a01fbbdf1e41e12313b95198eb15
Nranphy <30568146+Nranphy@users.noreply.github.com>
2022-02-18 14:02:34 +0800

    Test.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from .config import Config
 from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.log import logger
 from .get_data import get_today_title,get_sub_problem_data
+from nonebot_plugin_htmlrender import get_new_page
+import os
 
 
 
@@ -21,4 +23,33 @@ async def send_today_problem(bot: Bot,event:Event):
     today_data = get_sub_problem_data(today_title)
     logger.info("获取题目内容成功。")
     logger.debug(f"题目{today_data[0]}的难度为{today_data[1]},内容略。")
-    await request_today.send("\n".join(today_data[:2])+today_data[2]+f"\n{today_data[3]}")
+
+    ##将html转为图片
+    html_path = f"data/nonebot_plugin_leetcode/html"
+    check_dir(html_path)
+    html_file_path = html_path+f'/{today_title}.html'
+    img_path = f"data/nonebot_plugin_leetcode/img"
+    check_dir(img_path)
+    img_file_path = img_path+f'/{today_title}.png'
+    with open(html_file_path,"w+") as f:
+        f.write(today_data[2])
+    try:
+        async with get_new_page(viewport={"width": 300, "height": 300}) as page:
+                await page.goto(
+                    "file://"+html_file_path,
+                    wait_until="networkidle"
+                )
+                pic = await page.screenshot(full_page=True, path=img_file_path)
+    except Exception as e:
+        logger.error("题目内容（html）转图片出错。")
+        raise e
+    await request_today.send("\n".join(today_data[:2])+pic+f"\n{today_data[3]}")
+
+
+
+#网上找来的，创建不存在的目录
+def check_dir(path):
+    '''判断文件夹是否存在，如果不存在就创建一个'''
+    if not os.path.isdir(path):
+        os.makedirs(path)
+        return "Create Successful."
--- get_data.py
+++ get_data.py
@@ -1,7 +1,6 @@
 import httpx
 import json
-import os
-from nonebot_plugin_htmlrender import get_new_page
+from nonebot.log import logger
 
 #获取今日的每日一题标题，之后再另查询内容
 def get_today_title():
@@ -14,6 +13,7 @@ def get_today_title():
         titleSlug = today_data["data"]["todayRecord"][0]["question"]["titleSlug"]
         return titleSlug
     except Exception as e:
+        logger.error("获取每日一题标题时出错。",e)
         raise e
 
 
@@ -38,39 +38,9 @@ def get_sub_problem_data(titleSlug_):
         problem_content = problem_data.get("translatedContent")
         ##去除转义
         problem_content.replace('\"','"')
-        ##将html转为图片
-        html_path = f"data/nonebot_plugin_leetcode/html"
-        check_dir(html_path)
-        with open(html_path+f"/{titleSlug_}.html","w+") as f:
-            f.write(problem_content)
-        problem_content = html2img(html_path+f"/{titleSlug_}.html",titleSlug_)
         #题目链接
         problem_link = "本题链接："+f"https://leetcode-cn.com/problems/{titleSlug_}/" 
         return [problem_title,problem_difficulty,problem_content,problem_link]
     except Exception as e:
+        logger.error("获取已知题目的内容时出错。",e)
         raise e
-
-    
-
-#直接用大佬的插件转化html！！！！yyds
-async def html2img(path,title):
-    try:
-        async with get_new_page(viewport={"width": 300, "height": 300}) as page:
-            await page.goto(
-                "file://"+path,
-                wait_until="networkidle"
-            )
-            img_path = f"data/nonebot_plugin_leetcode/img"
-            check_dir(img_path)
-            pic = await page.screenshot(full_page=True, path=img_path+f"/{title}.png")
-            return pic
-    except Exception as e:
-        raise e
-
-
-#网上找来的，创建不存在的目录
-def check_dir(path):
-    '''判断文件夹是否存在，如果不存在就创建一个'''
-    if not os.path.isdir(path):
-        os.makedirs(path)
-        return "Create Successful."
