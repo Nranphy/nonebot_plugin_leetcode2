3cb916df7cd1db84690c85a6fc60a796bf0a4ea5
Nranphy <30568146+Nranphy@users.noreply.github.com>
2022-02-24 02:18:46 +0800

    Test.

--- __init__.py
+++ __init__.py
@@ -33,20 +33,12 @@ async def send_today_problem(bot: Bot,event:Event):
     with open(html_file_path,"w+") as f:
         f.write(today_data[2])
     try:
-        # async with get_new_page(viewport={"width": 300, "height": 300}) as page:
-        #         await page.goto(
-        #             "file://"+str(os.getcwd())+"/"+html_file_path,
-        #             wait_until="networkidle"
-        #         )
-        #         pic = await page.screenshot(full_page=True, path=img_file_path)
-        #干脆直接访问×
-        async with get_new_page(viewport={"width": 500, "height": 300}) as page:
+        async with get_new_page(viewport={"width": 300, "height": 300}) as page:
                 await page.goto(
-                    "https://leetcode-cn.com/problems/"+today_title,
+                    "file://"+str(os.getcwd())+"/"+html_file_path,
                     wait_until="networkidle"
                 )
-                div = page.locator(".content__1Y2H")
-                pic = await div.screenshot(path=img_file_path)
+                pic = await page.screenshot(full_page=True, path=img_file_path)
     except Exception as e:
         logger.error("题目内容（html）转图片出错。")
         raise e
