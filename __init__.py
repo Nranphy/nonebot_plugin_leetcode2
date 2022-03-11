ebe67717747464bb32eeb6994bf4124ae79aa135
Nranphy <30568146+Nranphy@users.noreply.github.com>
2022-03-11 22:07:54 +0800

    Changed little.

--- __init__.py
+++ __init__.py
@@ -169,16 +169,22 @@ async def send_user_data(bot: Bot,event:Event,  state: T_State = State()):
         user_public_profile = get_user_public_profile(state["user_Slug"])
         user_question_progress = get_user_question_progress(state["user_Slug"])
         user_solution_count = get_user_solution_count(state["user_Slug"])
-        # user_profile_articles = get_user_profile_articles(state["user_Slug"])
+        user_profile_articles = get_user_profile_articles(state["user_Slug"])
         user_community_achievement = get_user_community_achievement(state["user_Slug"])
-        # user_question_submitstats = get_user_question_submitstats(state["user_Slug"])
-        # user_medals = get_user_medals(state["user_Slug"])
+        user_question_submitstats = get_user_question_submitstats(state["user_Slug"])
+        user_medals = get_user_medals(state["user_Slug"])
     except Exception as e:
         await request_user.finish("连接到leetcode失败...呜呜呜...\n请稍后再试！！")
     try:
         userSlug = state["user_Slug"]
         realName = user_public_profile["data"]["userProfilePublicProfile"]["profile"]["realName"]
         userAvatar = httpx.get(user_public_profile["data"]["userProfilePublicProfile"]["profile"]["userAvatar"])
+        totalSubmissions = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["totalSubmissions"]
+        waSubmissions = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["waSubmissions"]
+        acSubmissions = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["acSubmissions"]
+        reSubmissions = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["reSubmissions"]
+        otherSubmissions = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["otherSubmissions"]
+        acTotal = user_public_profile["data"]["userProfilePublicProfile"]["submissionProgress"]["acTotal"]
         logger.debug("user_public_profile数据解析完成")
 
         voteCount = user_community_achievement["data"]["profileCommunityAchievement"][0]["voteCount"]
@@ -189,15 +195,54 @@ async def send_user_data(bot: Bot,event:Event,  state: T_State = State()):
         numAcceptedQuestions_easy = user_question_progress["data"]["userProfileUserQuestionProgress"]["numAcceptedQuestions"][0]["count"]
         numAcceptedQuestions_medium = user_question_progress["data"]["userProfileUserQuestionProgress"]["numAcceptedQuestions"][1]["count"]
         numAcceptedQuestions_difficulty = user_question_progress["data"]["userProfileUserQuestionProgress"]["numAcceptedQuestions"][2]["count"]
+        numFailedQuestions_easy = user_question_progress["data"]["userProfileUserQuestionProgress"]["numFailedQuestions"][0]["count"]
+        numFailedQuestions_medium = user_question_progress["data"]["userProfileUserQuestionProgress"]["numFailedQuestions"][1]["count"]
+        numFailedQuestions_difficulty = user_question_progress["data"]["userProfileUserQuestionProgress"]["numFailedQuestions"][2]["count"]
+        numUntouchedQuestions_easy = user_question_progress["data"]["userProfileUserQuestionProgress"]["numUntouchedQuestions"][0]["count"]
+        numUntouchedQuestions_medium = user_question_progress["data"]["userProfileUserQuestionProgress"]["numUntouchedQuestions"][1]["count"]
+        numUntouchedQuestions_difficulty = user_question_progress["data"]["userProfileUserQuestionProgress"]["numUntouchedQuestions"][2]["count"]
         logger.debug("user_question_progress数据解析完成")
 
         columnsUserSolutionCount = user_solution_count["data"]["columnsUserSolutionCount"]
         logger.debug("user_solution_count数据解析完成")
 
+        acSubmissionNum_easy = user_question_submitstats["data"]["userProfileUserQuestionSubmitStats"]["acSubmissionNum"][0]["count"]
+        acSubmissionNum_medium = user_question_submitstats["data"]["userProfileUserQuestionSubmitStats"]["acSubmissionNum"][1]["count"]
+        acSubmissionNum_hard = user_question_submitstats["data"]["userProfileUserQuestionSubmitStats"]["acSubmissionNum"][2]["count"]
+        logger.debug("user_question_submitstats数据解析完成")
+
+        if user_medals["data"]["userProfileUserMedals"]:
+            latest_madal_name = user_medals["data"]["userProfileUserMedals"][0]["name"]
+            latest_madal_date = user_medals["data"]["userProfileUserMedals"][0]["obtainDate"]
+            latest_madal = f"【最近勋章】  勋章名：{latest_madal_name} |获得时间：{latest_madal_date}\n"
+        else:
+            latest_madal = ""
+        logger.debug("user_medals数据解析完成")
+
+        if user_profile_articles["data"]["solutionArticles"]["edges"]:
+            latest_article_title = user_profile_articles["data"]["solutionArticles"]["edges"][0]["node"]["title"]
+        else:
+            latest_article_title = ""
+        logger.debug("user_profile_articles数据解析完成")
+
+
     except Exception as e:
         await request_user.finish("解析用户信息出错×\n用户ID错误或不存在，请输入用户ID而非用户昵称哦~")
 
-    await request_user.send("用户查询数据成功~\n"+MessageSegment.image(userAvatar.read())+f"用户昵称：{realName}（{userSlug}）\n========\n"+f"【已解决问题】  EASY：{numAcceptedQuestions_easy} |MEDIUM：{numAcceptedQuestions_medium} |DIFFICULTY：{numAcceptedQuestions_difficulty}\n========\n"+f"【成就贡献】  阅读总数：{viewCount} |获得点赞：{voteCount} |获得收藏：{favoriteCount} |发布题解：{columnsUserSolutionCount}"+f"\n用户主页：https://leetcode-cn.com/u/{userSlug}/")
+    await request_user.send(\
+        "用户查询数据成功~\n"+\
+        MessageSegment.image(userAvatar.read())+\
+        f"用户名：{realName}（{userSlug}）\n"+\
+            "========\n"+\
+            f"【已解决问题】  EASY：{numAcceptedQuestions_easy} |MEDIUM：{numAcceptedQuestions_medium} |DIFFICULTY：{numAcceptedQuestions_difficulty}\n"+\
+            f"【未答出问题】  EASY：{numFailedQuestions_easy} |MEDIUM：{numFailedQuestions_medium} |DIFFICULTY：{numFailedQuestions_difficulty}\n"+\
+            f"【未接触问题】  EASY：{numUntouchedQuestions_easy} |MEDIUM：{numUntouchedQuestions_medium} |DIFFICULTY：{numUntouchedQuestions_difficulty}\n"+\
+            f"【历史提交】  提交总数：{totalSubmissions} |错误提交数：{waSubmissions} |正确提交数：{acSubmissions} |重新提交数：{reSubmissions} |其他提交数：{otherSubmissions} |AC总数：{acTotal}\n"+\
+            f"【提交通过数】  EASY：{acSubmissionNum_easy} |MEDIUM：{acSubmissionNum_medium} |DIFFICULTY：{acSubmissionNum_hard}\n"+\
+            "========\n"
+            f"【成就贡献】  阅读总数：{viewCount} |获得点赞：{voteCount} |获得收藏：{favoriteCount} |发布题解：{columnsUserSolutionCount}\n"+\
+            latest_madal+latest_article_title+\
+            f"\n用户主页：https://leetcode-cn.com/u/{userSlug}/")
 
 
 
